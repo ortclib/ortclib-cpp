20165315e8bf770f00c2bf8ca311a456c68777e4
Robin Raymond <robin@opticaltone.com>
2016-11-22 21:05:11 -0500

    - reworked some of the Xcode projects to be more proper

---
+++ projects/xcode/ortclib.Test-ios/ortclib.Test-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ortclib.Test-ios
+//
+//  Created by Robin Raymond on 2016-11-22.
+//  Copyright © 2016 Open Peer Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ projects/xcode/ortclib.Test-ios/ortclib.Test-iosTests/ortclib_Test_iosTests.m
@@ -0,0 +1,39 @@
+//
+//  ortclib_Test_iosTests.m
+//  ortclib.Test-iosTests
+//
+//  Created by Robin Raymond on 2016-11-22.
+//  Copyright © 2016 Open Peer Foundation. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ortclib_Test_iosTests : XCTestCase
+
+@end
+
+@implementation ortclib_Test_iosTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
